8be14ccab9959a622626221cf9102f08f9817c88
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2026-06-08 20:53:08 +0530

    with Id

--- src/main/java/com/budget/controller/ContactsController.java
+++ src/main/java/com/budget/controller/ContactsController.java
@@ -3,11 +3,14 @@ package com.budget.controller;
 import com.budget.model.Contacts;
 import com.budget.service.ContactsService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class ContactsController {
 
     private final ContactsService service;
@@ -19,5 +22,10 @@ public class ContactsController {
     @GetMapping("/contacts")
     public List<Contacts> getAllContacts() {
         return service.getAllContacts();
+    }
+
+    @GetMapping("/contacts/{id}")
+    public Contacts getContactById(@PathVariable Long id) {
+        return service.getContactById(id);
     }
 }
