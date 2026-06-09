1b2460281b427f008b8a7b9933936513f9338ed8
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2026-06-09 21:20:09 +0530

    working build for contacts

--- src/main/java/com/budget/model/Contacts.java
+++ src/main/java/com/budget/model/Contacts.java
@@ -2,7 +2,6 @@ package com.budget.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
--- src/main/java/com/budget/repository/ContactsRepository.java
+++ src/main/java/com/budget/repository/ContactsRepository.java
@@ -3,6 +3,6 @@ package com.budget.repository;
 import com.budget.model.Contacts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ContactsRepository extends JpaRepository<Contacts, Long> {
-//    Optional<Contacts> contact = repository.findById(5L);
 }
